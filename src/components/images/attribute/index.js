522c16d143b1b4dd2005b72df125e6effd75c170
SherwinKhoo <99573291+SherwinKhoo@users.noreply.github.com>
2022-03-25 19:20:10 +0800

    Added images for attributes

---
+++ src/components/images/attribute/index.js
@@ -0,0 +1,8 @@
+const ATTR = {
+  str: require("./strength.webp"),
+  agi: require("./agility.webp"),
+  int: require("./intelligence.webp"),
+  Melee: require("./ranged.webp"),
+  Ranged: require("./melee.webp"),
+};
+export default ATTR;
